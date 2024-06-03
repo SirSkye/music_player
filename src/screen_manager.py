f221b4ecffb945e579232f8e21fdd715e482c24a
SirSkye <?>
2024-06-02 23:03:15 -0400

    Changed screen manager

--- src/screen_manager.py
+++ src/screen_manager.py
@@ -1,23 +1,23 @@
 class ScreenManager:
     def __init__(self, app):
         self.app = app
-        self.states = {}
-        self.state = None
+        self.screens = dict()
+        self.current_screen = None
 
-    def add(self, state):
-        self.states[state.name] = state
+    def add(self, screen):
+        self.screens[screen.name] = screen
 
-    def set_state(self, name):
-        self.state = self.states[name]
+    def set_screen(self, name):
+        self.current_screen = self.screens[name]
 
     def update(self):
-        if self.state is not None:
-            self.state.update()
+        if self.current_screen is not None:
+            self.current_screen.update()
 
     def draw(self):
-        if self.state is not None:
-            self.state.draw()
+        if self.current_screen is not None:
+            self.current_screen.draw()
 
     def handle_events(self, event):
-        if self.state is not None:
-            self.state.handle_events(event)
+        if self.current_screen is not None:
+            self.current_screen.handle_events(event)
