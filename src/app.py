e8d0fdd94c9045694fd259768e2e3b061982fad8
SirSkye <?>
2024-06-09 21:24:08 -0400

    Mostly done with the playerFrame

--- src/app.py
+++ src/app.py
@@ -1,52 +1,85 @@
-import pygame
-from screen_manager import ScreenManager
-from download_screen import DownloadScreen
-from player_screen import PlayerScreen
+import tkinter as tk
+from tkinter import ttk
+from utils import open_img
+from player import Player
 
-class App:
-    def __init__(self):
-        pygame.init()
+class App(tk.Tk):
+    def __init__(self, assets_dir: str):
+        super().__init__()
+        self.title("Music Player")
+        self.geometry(f"600x700")
+        self.resizable(False, False)
 
-        self.width, self.height = self.size = (800, 600)
+        s = ttk.Style()
+        s.configure("first.TFrame", background = "blue")
+        s.configure("second.TFrame", background = "red")
+        s.configure("noFocus.TButton", relief="flat", background="SystemButtonFace", borderwidth=0)
+        frame1 = ttk.Frame(self, height=600, width=600, style="first.TFrame")
+        frame1.pack()
 
-        self.screen = pygame.display.set_mode(self.size)
-        pygame.display.set_caption("Music Player")
+        self.frame = PlayerFrame(self, assets_dir)
 
-        self.screen_manager = ScreenManager(self)
 
-        self.screen_manager.add(DownloadScreen("downloader", self))
-        self.screen_manager.add(PlayerScreen("player", self))
+class PlayerFrame(ttk.Frame):
+    def __init__(self, parent, assets_dir:str):
+        self.player = Player(r"C:\Users\aisha\garbage\music_player-1\music")
+        ttk.Frame.__init__(self, parent, height=100, width=600, style="second.TFrame")
+        self.pack(fill = "both", expand=True)
+        self.columnconfigure([x for x in range(20)], weight=1, uniform="a")
+        self.rowconfigure([x for x in range(5)], weight=1, uniform="b")
 
-        self.screen_manager.set_state("downloader")
+        ttk.Scale(self, command=lambda value: self.player.set_vol(1 - float(value)), orient="vertical").grid(column=19, row=0, rowspan=4, sticky="ns")
+        img = open_img(None, fr"{assets_dir}\sound_icon.png")
+        sound_label = ttk.Label(self, image=img, anchor="center", background="red")
+        sound_label.grid_propagate(False)
+        sound_label.image = img
+        sound_label.grid(column=19, row=4, sticky="nsew")
 
-        self.fps = 60
+        self.song_info_frame = ttk.Frame(self, style="first.TFrame")
+        self.song_info_frame.pack_propagate(False)
+        self.song_info_frame.grid(column=0, row=3, columnspan=19, rowspan=2, sticky="nesw")
+        self.song_title_label = ttk.Label(self.song_info_frame, text = self.change_label_text("eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeb"))
+        self.song_title_label.pack()
+        self.song_artist_label = ttk.Label(self.song_info_frame, text = "eeeeeeeee")
+        self.song_artist_label.pack()
 
-        self.clock = pygame.time.Clock()
-        self.running = True
+        self.song_progress = ttk.Progressbar(self, value=0, maximum = 1, phase=True)
+        self.song_progress.grid(column=0, columnspan=19, row=2, sticky="ew")
+        self.after(1, self.updt_song_time)
+        self.play_button = ttk.Button(self, compound="top", style="noFocus.TButton")
+        self.play_button.grid_propagate(False)
+        self.play_button.grid(column=9, row=1, sticky="nesw")
+        self.pause_img = open_img((30, 15), fr"{assets_dir}\pause_icon.png")
+        self.play_img = open_img((30, 15), fr"{assets_dir}\play_icon.png")
+        self.play_button["image"] = self.play_img 
+        self.next_img = open_img((30, 15), fr"{assets_dir}\next_icon.png")
+        self.back_img = open_img((30, 15), fr"{assets_dir}\back_icon.png")
+        self.next_button = ttk.Button(self, compound="top", image = self.next_img, style="noFocus.TButton")
+        self.next_button.grid(column=10, row =1, sticky="nesw")
+        self.back_button = ttk.Button(self, compound="top", image = self.back_img)
+        self.back_button.grid(column=8, row=1, sticky="nsew")
 
-        self.delta_time = 0
+        self.after(50, self.check_updt_song)
+    
+    def check_updt_song(self):
+        self.updt_song_time()
+        self.song_title_label["text"] = self.change_label_text(self.player.current.name)
+        self.song_artist_label["text"] = self.change_label_text(self.player.current.artist)
+        self.after(50, self.check_updt_song)
 
-    def run(self):
-        while self.running:
-            self.delta_time = self.clock.tick(self.fps) / 1000.0
-            self.events()
-            self.update()
-            self.draw()
+    def change_label_text(self, text: str) -> str:
+        if len(text) <= 94:
+            return text
+        return text[:92] + "..."
+    
+    def updt_song_time(self):
+        self.player.current.updt_time()
+        self.song_progress["value"] = self.player.current.time/self.player.current.length
 
-    def events(self):
-        events = pygame.event.get()
-
-        for event in events:
-            if event.type == pygame.QUIT:
-                self.running = False
-
-            self.screen_manager.handle_events(event)
-
-    def update(self):
-        self.screen_manager.update()
-
-    def draw(self):
-        self.screen.fill("white")
-
-        self.screen_manager.draw()
-        pygame.display.flip()
+    def load_song(self, text: str) -> str:
+        self.song_title_label(self.change_label_text("Best Of You"))
+        self.song_artist_label(self.change_label_text(self.player.get_artist("Best Of You")))
+        self.player.load_song("Best of You")
+        
+app = App(r"C:\Users\aisha\garbage\music_player-1\assets")
+app.mainloop()
